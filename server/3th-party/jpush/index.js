74eaf2168e777244faa7bff7e44fe8914f141dda
傅文松 <908159107@qq.com>
2016-06-26 00:55:21 +0800

    添加拍卖师审核通过、不通过的推送

--- server/3th-party/jpush/index.js
+++ server/3th-party/jpush/index.js
@@ -8,7 +8,36 @@ module.exports = function (appKey, masterSecret, retryTimes, isDebug) {
     notificationAuktionatorCheckProduct: function (auktionatorId, cb) {
       client.push().setPlatform(JPush.ALL)
         .setAudience(JPush.alias(auktionatorId.toString()))
-        .setNotification('尊敬的拍卖师，您有拍品需要审核，点击查看')
+        .setNotification(
+          JPush.android('尊敬的拍卖师！您有拍品需要审核，点击查看详情', null, null, {code: 100}),
+          JPush.ios('尊敬的拍卖师！您有拍品需要审核，点击查看详情', null, null, null, {code: 100})
+        )
+        .send(function(err, res) {
+          if (err) return console.error('推送失败：', err.message);
+          cb && cb(null, res);
+        });
+    },
+    // 通知卖家审核通过
+    notificationSellerProductPassed: function (ownerId, cb) {
+      client.push().setPlatform(JPush.ALL)
+        .setAudience(JPush.alias(ownerId.toString()))
+        .setNotification(
+          JPush.android('恭喜您！您的拍品已审核通过，赶紧点击查看详情', null, null, {code: 101}),
+          JPush.ios('恭喜您！您的拍品已审核通过，赶紧点击查看', null, null, null, {code: 101})
+        )
+        .send(function(err, res) {
+          if (err) return console.error('推送失败：', err.message);
+          cb && cb(null, res);
+        });
+    },
+    // 通知卖家审核未通过
+    notificationSellerProductRejected: function (ownerId, cb) {
+      client.push().setPlatform(JPush.ALL)
+        .setAudience(JPush.alias(ownerId.toString()))
+        .setNotification(
+          JPush.android('很遗憾！您的拍品未能审核通过，点击查看详情', null, null, {code: 102}),
+          JPush.ios('很遗憾！您的拍品未能审核通过，点击查看详情', null, null, null, {code: 102})
+        )
         .send(function(err, res) {
           if (err) return console.error('推送失败：', err.message);
           cb && cb(null, res);
--- server/models/ak-product.js
+++ server/models/ak-product.js
@@ -6,7 +6,18 @@ module.exports = function(AkProduct) {
     if (ctx.instance) {
       // 拍品创建
       var product = ctx.instance;
-      thirdParties.JPush.notificationAuktionatorCheckProduct(product.auktionatorId);
+
+      switch (product.status) {
+        case 0: // 创建
+          thirdParties.JPush.notificationAuktionatorCheckProduct(product.auktionatorId);
+          break;
+        case 1: // 拍卖师审核通过
+          thirdParties.JPush.notificationSellerProductPassed(product.ownerId);
+          break;
+        case 10: // 拍卖师审核不通过
+          thirdParties.JPush.notificationSellerProductRejected(product.ownerId);
+          break;
+      }
     } else {
       console.log(ctx.Model.pluralModelName, ctx.where);
     }
